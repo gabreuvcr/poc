1fe94c1f9da4ab920ab65aea86969c2fcf10fc88
Gabriel Rocha <gabreuvcr@gmail.com>
2023-04-01 00:35:06 -0300

    Atualizando os scripts de plot

---
+++ utils/de_hs_ploting.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+from sys import argv
+
+def de_get_output_splited(line):
+    line = line.split(';')
+    x, y = [], []
+    for numbers in line:
+        if '\n' in numbers: continue
+        n = numbers.split('|')
+        x += [float(n[0])]
+        y += [float(n[1])]
+    return x, y
+
+
+def hs_get_output_splited(line):
+    line = line.split(';')
+    x, y_min, y_avg, y_max = [], [], [], []
+    for numbers in line:
+        if '\n' in numbers: continue
+        n = numbers.split('|')
+        x += [float(n[0])]
+        y_min += [float(n[1])]
+        y_avg += [float(n[2])]
+        y_max += [float(n[3])]
+    return x, y_min, y_avg, y_max
+
+
+sizes = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+colors = [
+    "#99F2D1", "#8BDEC0", "#7DCAAE", "#6FB69D", "#61A28C",
+    "#548E7A", "#467A69", "#386658", "#2A5246", "#1C3E35"
+]
+
+de_output_file = argv[1]
+hs_output_file = argv[2]
+path_save = argv[3]
+max_sensors = -1;
+
+
+de_file = open(de_output_file, 'r')
+hs_file = open(hs_output_file, 'r')
+x_de, y_de = de_get_output_splited(de_file.readlines()[10])
+x_hs, _, y_hs_avg, _ = hs_get_output_splited(hs_file.readlines()[0])
+max_sensors = max(max(x_de), max(x_hs))
+
+## color
+# plt.scatter(x_de, y_de, s=50, color=colors[9], marker='o', label='Differential Evolution')
+# plt.scatter(x_hs, y_hs_avg, s=50, color=colors[2], marker='o', label='Harmony Search')
+## bw
+plt.scatter(x_de, y_de, s=40, facecolors='none', edgecolors='black', marker='o', label='Differential Evolution')
+plt.scatter(x_hs, y_hs_avg, s=15, color='black', marker='.', label='Harmony Search')
+
+plt.title("Comparação DE e HS: Instância 1")
+plt.legend()    
+plt.xlim(0, max_sensors + 1)
+plt.ylim(0, 1.1)
+plt.xlabel('Número de sensores')
+plt.ylabel('% de cobertura')
+plt.hlines(y=1, linestyles='--', xmin=0, xmax=max_sensors+1, colors='#000', alpha=0.15)
+plt.tight_layout()
+plt.savefig(path_save, dpi=300)
--- utils/de_ploting.py
+++ utils/de_ploting.py
@@ -1,53 +1,53 @@
 import matplotlib.pyplot as plt
 from sys import argv
 
-def get_output_splited(file):
-    text = file.readline()
-    line = text.split(';')
+def get_output_splited(line):
+    line = line.split(';')
     x, y = [], []
-    for i in range(0, len(line)):
-        if '\n' in line[i]: continue
-        l = line[i].split('|')
-        x += [float(l[0])]
-        y += [float(l[1])]
+    for numbers in line:
+        if '\n' in numbers: continue
+        n = numbers.split('|')
+        x += [float(n[0])]
+        y += [float(n[1])]
     return x, y
 
-def plot_scatter(x, y, max_sensors, label, marker, size, color):
-    plt.scatter(x, y, s=size, label=label, marker=marker, color=color, edgecolor='None')
-    plt.legend()
-    plt.xlim(0, max_sensors + 1)
-    plt.ylim(0, 1.1)
-    plt.xlabel('Número de sensores')
-    plt.ylabel('% de cobertura')
 
-def plot_line(x, y, max_sensors, label):
-    plt.plot(x, y, label=label, linestyle=':')
-    plt.legend()
-    plt.xlim(0, max_sensors + 1)
-    plt.ylim(0, 1.1)
-    plt.xlabel('Número de sensores')
-    plt.ylabel('% de cobertura')
+sizes = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+sizes2 = [190, 170, 150, 130, 110, 90, 70, 50, 30, 10]
+colors = [
+    "#99F2D1", "#8BDEC0", "#7DCAAE", "#6FB69D", "#61A28C", 
+    "#548E7A", "#467A69", "#386658", "#2A5246", "#1C3E35"
+]
 
-max_sensors = -1;
 output_file = argv[1]
 path_save = argv[2]
+max_sensors = -1;
 
 f = open(output_file, 'r')
-markers = ['o', 'v', 's', 'p', '*', 'h', 'H', 'D', 'd', 'P', 'X']
-sizes = [100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
-colors = ["#99F2D1", "#8BDEC0", "#7DCAAE", "#6FB69D", "#61A28C", "#548E7A", "#467A69", "#386658", "#2A5246", "#1C3E35"]
-bw = ["#000", "#161616", "#2C2C2C", "#424242", "#585858", "#6E6E6E", "#848484", "#9A9A9A", "#B0B0B0", "#C6C6C6"]
-# bw = ["#C6C6C6", "#B0B0B0", "#9A9A9A", "#848484", "#6E6E6E", "#585858", "#424242", "#2C2C2C", "#161616", "#000"]
-
+lines = f.readlines()
 for i in range(0, 10):
-    x_de, y_de = get_output_splited(f)
-    print(x_de)
-    print(y_de)
+    x_de, y_de = get_output_splited(lines[i])
     max_sensors = max(max(x_de), max_sensors)
-    # plot_scatter(x_de, y_de, max_sensors, f'{i + 1}', marker=markers[0], size=sizes[i], color=bw[i])
-    plot_scatter(x_de, y_de, max_sensors, f'{i + 1}', marker=markers[0], size=sizes[i], color=colors[i])
+    
+    # ## bw
+    # plt.scatter(
+    #     x_de, y_de, s=sizes2[i], facecolors='none', 
+    #     edgecolors='black', marker='o', label=f'{i + 1}', linewidths=0.5
+    # )
+    ## color
+    plt.scatter(
+        x_de, y_de, s=sizes[i], marker='o',
+        label=f'{i + 1}', color=colors[i], edgecolor='None'
+    )
+    # ## line
+    # plt.plot(x_de, y_de, label=f'{i + 1}', linestyle=':')
 
-plt.title("")
-plt.tight_layout()
+plt.title("Differential Evolution: Instância 1")
+plt.legend()
+plt.xlim(0, max_sensors + 1)
+plt.ylim(0, 1.1)
+plt.xlabel('Número de sensores')
+plt.ylabel('% de cobertura')
 plt.hlines(y=1, linestyles='--', xmin=0, xmax=max_sensors+1, colors='#000', alpha=0.15)
+plt.tight_layout()
 plt.savefig(path_save, dpi=300)
